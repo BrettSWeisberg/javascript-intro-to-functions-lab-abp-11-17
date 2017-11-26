27b4c9c154962f2d4d03ae55b1467dee0234ba1f
brett weisberg <weisberg.brett@gmail.com>
2017-11-26 22:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
   }
 
-function sayHiToGrandma(string) {
-  return string.toLowerCase();
+function sayHiToGrandma('hello') {
+  return string.toLowerCase()
 }
